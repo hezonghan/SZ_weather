260ffe32a67e1709688631676c1449fb8c6103e8
hezonghan <11812917@mail.sustech.edu.cn>
2021-07-24 14:16:58 +0800

    commit before re-locating local repo

---
+++ get_warnings.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+	"math/rand"
+)
+
+func main(){
+	random_value := rand.Float32()
+	time_value := time.
+	url := 'https://weather.121.com.cn/data_cache/szWeather/alarm/szAlarm.js?random=' + random_value \
+               + '&_=' + str(round(time_value * 1000))
+}
